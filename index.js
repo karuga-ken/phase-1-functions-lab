e3736ca43fa60710ad66e2431de1124f5966e302
Karuga Mbugua <karugambugua@icloud.com>
2023-10-04 13:58:20 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(block){
+    if (block > 42) {
+        return block - 42;
+    }else{
+        return 42 - block;
+    }
+}
+
+function distanceFromHqInFeet (block){
+        return distanceFromHqInBlocks(block) * 264;
+}
+
+function distanceTravelledInFeet (location, destination) {
+    if (location < destination){
+        return (destination - location) * 264;
+    }else{
+        return (location - destination) *264;
+    }
+}
+
+function calculatesFarePrice (location, destination){
+    const distance = distanceTravelledInFeet (location, destination);
+    if (distance <= 400 ){
+        return 0;
+    }else if(distance > 400 && distance <= 2000){
+        return (distance - 400 ) * 0.02;
+    }else if(distance > 2000 && distance < 2500){
+        return 25;
+    }else{
+        return "cannot travel that far"
+    }
+}
